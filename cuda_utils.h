86c3fed342896524b41fd54fdafd5cef4274b7f7
adrk17 <adrian.krupa17@gmail.com>
2025-05-18 16:23:28 +0200

    Init

---
+++ cuda_utils.h
@@ -0,0 +1,18 @@
+#ifndef CUDA_UTILS_H
+#define CUDA_UTILS_H
+
+#include <cuda_runtime.h>
+#include <iostream>
+#include <cstdlib>
+
+#define CUDA_CHECK(call)                                                        \
+    do {                                                                        \
+        cudaError_t err = call;                                                 \
+        if (err != cudaSuccess) {                                               \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__        \
+                      << " - " << cudaGetErrorString(err) << std::endl;         \
+            std::exit(EXIT_FAILURE);                                            \
+        }                                                                       \
+    } while (0)
+
+#endif
---
+++ kernel.cu
@@ -0,0 +1,31 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <opencv2/opencv.hpp>
+
+// Macro for checking CUDA errors
+#define CUDA_CHECK(call)                                                        \
+    do {                                                                        \
+        cudaError_t err = call;                                                 \
+        if (err != cudaSuccess) {                                               \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__        \
+                      << " - " << cudaGetErrorString(err) << std::endl;         \
+            std::exit(EXIT_FAILURE);                                            \
+        }                                                                       \
+    } while (0)
+
+#define TILE_WIDTH 16
+
+
+__global__ void kernel(int *c, const int *a, const int *b)
+{
+
+}
+
+int main() {
+    cv::Mat img(100, 100, CV_8UC1, cv::Scalar(128));
+    cv::imshow("Test", img);
+    cv::waitKey(0);
+    return 0;
+}
+
